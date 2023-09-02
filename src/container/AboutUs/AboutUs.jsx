daf8c091d7dc18f303e174c4bddb2b8fe9f8c0be
Genevieve Senecal <genevievesen23@gmail.com>
2023-09-02 15:49:37 -0400

    Update About us formatting + info

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -11,14 +11,17 @@ const AboutUs = () => (
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
-        <p className="p__opensans">Insert about us paragrpah</p>
+        <p className="p__opensans">The Cambridge Ski Association (CSA) is a group of elite skiers & snowboarders based out of the Cambridge, MA area.
+                                   We ski hard & party harder. The association is bi-coastal and multi-national. In perpetual search of fresh pow & good vibes. </p>
         <button type="button" className="custom__button">Learn More</button>
       </div>
-      {/* <div className="app__aboutus-content_history">
+      <div className="app__aboutus-content_history"></div>
+      <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
-        <p className="p__opensans">Insert our history paragraph</p>
+        <p className="p__opensans">Cambridge Ski Association history is rooted in East Coast Mountain culture. Get up early, ski hard and then Apres harder. 
+                                   Outside of the peak season we live by the motto "One Day Closer" to snow.</p>
         <button type="button" className="custom__button">Learn More</button>
-      </div> */}
+      </div>
 
     </div>
   </div>
