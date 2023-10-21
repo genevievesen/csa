4326ea55a6f696774cbf65886810f5704cf7376a
Genevieve Senecal <genevievesen23@gmail.com>
2023-10-21 15:31:08 -0400

    V2 complete

--- src/container/Members/Members.jsx
+++ src/container/Members/Members.jsx
@@ -7,27 +7,44 @@ import './Members.css';
 const Members = () => (
    <div className="app__members app__bg flex__center section__padding" id="members">
       <div className="app__members-title">
-         <h1 className="headtext__cormorant"> Founding Members</h1>
-         <img src={images.founders} alt="alt_founders"></img>
-      </div>
-      <div className="app__members-title">
-         <h1 className="headtext__cormorant"> Executive Team</h1>
-         <div className="app__members-exec-list">
-            {members.exec_team.map((exec_team, index) => (
-               < Exec keys={exec_team.name + index} image ={exec_team.image} name={exec_team.name} title={exec_team.title}/>
-                ))}
+      <h1 className="headtext__cormorant"> Members</h1>
+         <div className="app__members-subtitle">
+         <p className="app__members-subtitle_heading"> Founders</p>
+            <img className="app__members-founders" src={images.founders} alt="alt_founders"></img>
          </div>
-      </div>
-      <div className="app__members-title">
-         <h1 className="headtext__cormorant">Members</h1>
-         <div className="app__members-list">
-            {members.members.map((members, index) => (
-               < Member keys={members.name + index} name={members.name}/>
+         <div className="app__members-subtitle">
+         <p className="app__members-subtitle_heading"> Executive Team</p>
+            <div className="app__members-exec-list">
+               {members.exec_team.map((exec_team, index) => (
+                < Exec keys={exec_team.name + index} image ={exec_team.image} name={exec_team.name} title={exec_team.title}/>
                 ))}
+            </div>            
          </div>
+         <div className="app__members-subtitle">
+         <p className="app__members-subtitle_heading"> Members List</p>
+            <div className="app__members-list">
+               {members.members.map((members, index) => (
+                < Member keys={members.name + index} name={members.name}/>
+               ))}
+            </div>
+         </div>                  
       </div>
    </div>
 
 );
+
+// const Members = () => (
+//    <div className="app__members app__bg flex__center section__padding" id="members">
+//       <div className="app__members-title">
+//       </div>
+//       <div className="app__members-title">
+
+//       </div>
+//       <div className="app__members-title">
+
+//       </div>
+//    </div>
+
+// );
 
 export default Members;
