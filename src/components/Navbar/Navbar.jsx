2b819ce709eaba03c00179b476791c7d667a286e
Genevieve Senecal <genevievesen23@gmail.com>
2023-11-04 12:28:48 -0400

    fix bg loading issue

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import './Navbar.css';
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
-    <nav className="app__navbar app__bg">
+    <nav className="app__navbar">
       <div className="app__navbar-logo">
         <img src={images.csa_logo} alt="app__logo" />
       </div>
